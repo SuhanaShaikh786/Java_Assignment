5704fdeeb7c84b8b3985bdd9ae5dadb8e13b3e6a
Suhana <shaikhsuhana786@gmail.com>
2024-09-30 23:00:06 +0530

    File Added

---
+++ Desktop/Java_Assignments/Day-1/Demo.java
@@ -0,0 +1,17 @@
+class Demo{
+	public static void main(String args[])
+	{
+           int number=-1;
+	  // int number=1;
+	   //int number=0;
+	 if(number  >   0){
+	     System.out.println(number  +   " Number  is Positive");
+ 	   }
+	   else if (number  <  0){
+	      System.out.println(number  +   " Number  is Negative");
+	     }	
+	 else{
+	              System.out.println(number  +  " Number is Zero");
+	          }
+	}	
+}

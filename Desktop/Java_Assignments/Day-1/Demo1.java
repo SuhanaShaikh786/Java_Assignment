00f5361f5d2cb1d281b5a07e1304f595afa2eb23
Suhana <shaikhsuhana786@gmail.com>
2024-09-30 23:04:26 +0530

    file two is add

---
+++ Desktop/Java_Assignments/Day-1/Demo1.java
@@ -0,0 +1,16 @@
+class Demo1{
+	public static void main(String args[]){
+	  int i=1;
+	//  int i=4;
+	  if(i%2==0)
+	  {
+	     System.out.println("Number is Even");
+	   }
+	  else{
+	    System.out.println("Number is odd");
+	}
+	}
+
+
+
+}

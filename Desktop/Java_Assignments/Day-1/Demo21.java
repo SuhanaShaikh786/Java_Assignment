c4d416f430d559435f8dca2448cab252cf97975a
Suhana <shaikhsuhana786@gmail.com>
2024-09-30 23:19:16 +0530

    code added

---
+++ Desktop/Java_Assignments/Day-1/Demo21.java
@@ -0,0 +1,15 @@
+class Demo21{
+	public static void main(String args[]){
+	  int i=4;
+	  if(i%2==0)
+	  {
+	     System.out.println("Number is Even");
+	   }
+	  else{
+	    System.out.println("Number is odd");
+	}
+	}
+
+
+
+}

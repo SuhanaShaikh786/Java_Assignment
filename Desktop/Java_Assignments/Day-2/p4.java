9faebce90ec65a6137d5c04778f2b2d6622e58ba
Suhana <shaikhsuhana786@gmail.com>
2024-10-01 22:31:49 +0530

    file p4 added day-2

---
+++ Desktop/Java_Assignments/Day-2/p4.java
@@ -0,0 +1,18 @@
+class p4{
+	public static void main(String args[]){
+	System.out.println("\n Divided by 3 OR 7 ");
+	for(int i=1 ; i < 20;i++)
+	{
+	   if(i % 3 == 0 || i % 7 == 0)
+	   {
+	      System.out.print(i + " ");
+	    }
+	}	 
+
+
+
+	}
+
+
+
+}

4587d1e69ea9dfba3534810d1cb34652ff7f141d
Suhana <shaikhsuhana786@gmail.com>
2024-10-01 22:30:04 +0530

    file p2 added day-2

---
+++ Desktop/Java_Assignments/Day-2/p2.java
@@ -0,0 +1,23 @@
+class p2{
+	public static void main(String args[]){
+	   int num=20;
+	   int i=1;
+	  System.out.println(" List of Odd Numbers from 1 to 20 ");
+	   while(i<=num)
+	   {
+	     /*  if(i%2==1)
+	       {
+	          System.out.println(i+" ");
+	           
+	       }*/
+	      if(i%2==0)
+	       {
+	          System.out.println(i+"   ");
+	           
+	       }
+	     i++;
+	    }
+    }
+
+
+}

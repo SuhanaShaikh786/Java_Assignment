ed1daaad39d0a1d18f579503f89c556577a91d8a
Suhana <shaikhsuhana786@gmail.com>
2024-10-01 22:31:04 +0530

    file p3 added day-2

---
+++ Desktop/Java_Assignments/Day-2/p3.java
@@ -0,0 +1,18 @@
+class p3{
+	public static void main(String args[]){
+	System.out.println("\n Divided by 3 & 7 ");
+	for(int i=1 ; i < 20;i++)
+	{
+	   if(i % 3 == 0 && i % 7 == 0)
+	   {
+	      System.out.print(i + " ");
+	    }
+	}	 
+
+
+
+	}
+
+
+
+}

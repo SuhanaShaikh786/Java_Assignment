ec55226bb03f404676eb34c704e9f4e7d0d20295
Suhana <shaikhsuhana786@gmail.com>
2024-10-01 22:32:24 +0530

    file p5 added day-2

---
+++ Desktop/Java_Assignments/Day-2/p5.java
@@ -0,0 +1,16 @@
+class p5{
+	public static void main(String args[]){
+	//System.out.println("\n Divided by 3 & 7 ");
+        int i = 1;
+        System.out.println("Number beetwen 1 and 20 ");
+           while(i<=20)
+		{
+	   		if(i % 3 == 0 && i % 7 == 0)
+	   		{
+	      		System.out.println(i );
+	   		 }
+
+                        i++;
+	        }	 
+	}
+}

d0dbb19620f08270bb7d25feb097c3605731db2e
Suhana <shaikhsuhana786@gmail.com>
2024-10-03 22:26:35 +0530

    Day-4 Files added

---
+++ Desktop/Java_Assignments/Day-4/p1.java
@@ -0,0 +1,19 @@
+class p1{
+	public static void main(String args[]){
+	char ch='A';
+	for(int i=1;i<=9;i++)
+	{
+	   if(i%2==1)
+	   {
+		System.out.print(ch + " ");
+           }
+	ch++;
+	}
+
+
+
+}
+
+
+
+}

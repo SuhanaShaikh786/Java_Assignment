71c16467adbc3a800958dc04292e5204b8604acb
Suhana <shaikhsuhana786@gmail.com>
2024-10-13 13:10:05 +0530

    Code added

---
+++ Desktop/Java_Assignments/Day-8/p2.java
@@ -0,0 +1,16 @@
+class p2{
+	public static void main(String agrs[]){
+	char ch = 'A';
+	for(int i=1;i<=4;i++)
+	{
+	   for(int j=1; j<=i; j++)
+	   {
+		System.out.print(ch+ " ");
+		ch++;
+	   }
+ 	System.out.println();
+	}
+
+  }	
+
+}
---
+++ Desktop/Java_Assignments/Day-8/p3.java
@@ -0,0 +1,14 @@
+class p3{
+	public static void main(String agrs[]){
+	for(int i=1;i<=4;i++)
+	{
+	   for(int j=1; j<=i; j++)
+	   {
+		System.out.print("01 ");
+	   }
+ 	System.out.println();
+	}
+
+  }	
+
+}

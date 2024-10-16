27b7e9382c100dce02b67b3bcd3122512f58f109
Suhana <shaikhsuhana786@gmail.com>
2024-10-16 23:09:56 +0530

    code Added

---
+++ Desktop/Java_Assignments/Day-11/p1.java
@@ -0,0 +1,11 @@
+class p1{
+	public static void main(String args[]){
+	for(int i=1;i<=10;i++)
+	{
+	    int square = i * i;
+	     System.out.println("Square of "+ i +": "   +square);
+	}
+
+    }
+
+}
---
+++ Desktop/Java_Assignments/Day-11/p2.java
@@ -0,0 +1,11 @@
+class p2{
+	public static void main(String args[]){
+	for(int i=1;i<=20;i++)
+	{
+	    int cube = i * i * i;
+	     System.out.println("Cube of "+ i +": "   +cube);
+	}
+
+    }
+
+}

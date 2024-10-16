f988432d397024c27ce33b8587262d90eb512297
Suhana <shaikhsuhana786@gmail.com>
2024-10-16 21:15:36 +0530

    day-10

---
+++ Desktop/Java_Assignments/Day-10/s1.java
@@ -0,0 +1,17 @@
+class s1{
+	public static void main(String args[]){
+	int start=7;
+	for(int i=1; i<= 4; i++)
+	{
+	 for(int j=1;j<=i;j++)
+	  {	if(j>1){
+		     start--;
+		}
+	     System.out.print(start + " ");
+	   }
+	System.out.println();
+
+	}
+  }
+
+}

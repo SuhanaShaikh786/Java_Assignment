e09129c2a91a99727c43fd4504ce974e8c132bd1
Suhana <shaikhsuhana786@gmail.com>
2024-10-22 08:29:48 +0530

    Code Added

---
+++ Desktop/Java_Assignments/Day-13/p4.java
@@ -0,0 +1,12 @@
+class p4{
+	public static void main(String args[]){
+		int a=5, factorial=1;
+		
+		for(int i=1;i<=a;i++){
+			 factorial *= i;
+		}
+		System.out.println("The factorial of " + a  + "  is :"  +factorial);
+	}
+
+
+}

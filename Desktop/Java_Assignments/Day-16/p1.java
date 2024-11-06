d633b1245174e014d90dcbdaf7ef2d40424d0562
Suhana <shaikhsuhana786@gmail.com>
2024-11-06 22:44:02 +0530

    Code Added

---
+++ Desktop/Java_Assignments/Day-16/p1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class p1{
+	public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	int sum=0;
+	System.out.println("Enter num:");
+	int num = sc.nextInt();
+	for(int i=1;i<=num/2;i++){
+		if(num%i==0)
+		{	
+			sum+=i;
+		}
+		
+	}
+	if(num > 0 && sum == num){
+		System.out.println("Perefect");
+	}
+	else{
+		System.out.println("Not Perefect");
+	}
+    }
+}
---
+++ Desktop/Java_Assignments/Day-16/p2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class p2{
+	public static void main(String args[]){
+	Scanner sc= new Scanner(System.in);
+	int fact;
+	System.out.println("Enter Number1:");
+	int num1 = sc.nextInt();
+	System.out.println("Enter Number2:");
+	int num2 = sc.nextInt();
+	for(int i=num1;i<=num2;i++){
+		fact = 1;
+		for(int j=1; j<=i;j++){
+		fact *=j;
+	}
+	System.out.println("Factorial of" + i + "=" +fact);
+
+
+	}
+	
+	}
+
+
+
+}
